51ac162bf4f53c59268482dfe265978bc5b98c08
Ayushkant3011 <ayushkant17@gmail.com>
2026-06-10 16:20:23 +0530

    thread-pool

---
+++ Async Code/Thread_pool.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+
+process.UV_THREADPOOL_SIZE = 8;
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) =>{
+    console.log("1-cryptoPBKDF2 done");
+});
+
+
+// fs.readFile("./file.txt", "utf-8", () =>{
+//     console.log("File reading done");
+// });
+
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) =>{
+    console.log("2-cryptoPBKDF2 Done");
+});
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) =>{
+    console.log("3-cryptoPBKDF2 Done");
+});
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) =>{
+    console.log("4-cryptoPBKDF2 Done");
+});
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) =>{
+    console.log("5-cryptoPBKDF2 Done");
+});
