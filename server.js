a3cd8d76a8f289c4e71c60ecedeb11bc121aaff6
Ayushkant3011 <ayushkant17@gmail.com>
2026-06-11 16:46:50 +0530

    node way to create or handle api

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const http = require("http"); // require("node:http") both are same
 
 const server = http.createServer(function (req,res){
 
+    if(req.url === "/getSecretData"){
+        res.end("no Secret");
+    }
+
     res.end("Hello World Folks"); //sendding the data back
 });
 
